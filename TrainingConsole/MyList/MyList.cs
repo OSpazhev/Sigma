1d09b7e55420afa22b8d9a7903cdc164044cbaf6
Oleksandr Spazhev <O.spazhev@gmail.com>
2018-07-04 16:19:38 +0300

    Add MyList(Generic List analogue).

---
+++ TrainingConsole/MyList/MyList.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+
+namespace MyList
+{
+    public class MyList<T>
+    {
+        public int Count { get; private set; }
+        public int Capacity { get => array.Length; }
+        private T[] array;
+
+        public T this[int index]
+        {
+            get
+            {
+                return array[index];
+            }
+
+            set
+            {
+                array[index] = value;
+            }
+        }
+
+        public MyList() : this(0) { }
+
+        public MyList(int size) : this(size, default(T)) { }
+
+        public MyList(int size, T value)
+        {
+            array = Enumerable.Repeat<T>(value, size).ToArray();
+            Count = size;
+        }
+
+        public void Add(T value)
+        {
+            if (Capacity == 0)
+            {
+                array = new T[4];
+            }
+            if (Count >= Capacity)
+            {
+                T[] newArray = new T[Capacity * 2 + 1];
+                Array.Copy(array, newArray, Count);
+                array = newArray;
+            }
+            array[Count++] = value;
+        }
+
+        public void ForEach(Action<T> action)
+        {
+            for(int i = 0; i < Count; i++)
+            {
+                action(array[i]);
+            }
+        }
+
+        public void AddRange(IEnumerable<T> range)
+        {
+            foreach(T value in range)
+            {
+                Add(value);
+            }
+        }
+
+        public void Clear()
+        {
+            array = new T[0];
+            Count = 0;
+        }
+
+        public ReadOnlyCollection<T> AsReadOnly()
+        {
+            return new ReadOnlyCollection<T>(array);
+        }
+
+        public bool Contains(T item)
+        {
+            foreach(T value in array)
+            {
+                if (value.Equals(item))
+                    return true;
+            }
+            return false;
+        }
+    }
+}
---
+++ TrainingConsole/MyList/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+namespace MyList
+{
+    class Program
+    {
+        public static void WriteElement(int value)
+        {
+            Console.Write($"{value} ");
+        }
+        public static void OutputMyList(MyList<int> myList)
+        {
+            Console.WriteLine($"myList size: {myList.Count}");
+            Action<int> myDelegate = WriteElement;
+            myList.ForEach(myDelegate);
+            Console.WriteLine();
+        }
+        
+        static void Main(string[] args)
+        {
+            var myList1 = new MyList<int>();
+            OutputMyList(myList1);
+
+            var myList2 = new MyList<int>(10);
+            OutputMyList(myList2);
+
+            var myList3 = new MyList<int>(10, 9);
+            OutputMyList(myList3);
+
+            myList1.Add(10);
+            OutputMyList(myList1);
+
+            myList2.AddRange(new int[] { 20, 20, 20 });
+            OutputMyList(myList2);
+
+            myList1.Clear();
+            OutputMyList(myList1);
+
+            Console.WriteLine(myList3.Contains(9));
+            OutputMyList(myList3);
+        }
+    }
+}
