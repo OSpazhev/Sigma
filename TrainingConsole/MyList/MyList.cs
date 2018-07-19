7d0c5251d3fce638dfc1872631b419c3558059d1
Oleksandr Spazhev <o.spazhev@gmail.com>
2018-07-19 16:07:56 +0300

    Complete MyList task.

--- TrainingConsole/MyList/MyList.cs
+++ TrainingConsole/MyList/MyList.cs
@@ -30,7 +30,7 @@ namespace MyList
 
         public MyList(int size, T value)
         {
-            array = Enumerable.Repeat<T>(value, size).ToArray();
+            array = Enumerable.Repeat(value, size).ToArray();
             Count = size;
         }
 
@@ -49,31 +49,59 @@ namespace MyList
             array[Count++] = value;
         }
 
-        public void ForEach(Action<T> action)
-        {
-            for(int i = 0; i < Count; i++)
-            {
-                action(array[i]);
-            }
-        }
-
         public void AddRange(IEnumerable<T> range)
         {
-            foreach(T value in range)
+            foreach (T value in range)
             {
                 Add(value);
             }
+        }
+
+        public ReadOnlyCollection<T> AsReadOnly()
+        {
+            return new ReadOnlyCollection<T>(array);
+        }
+
+        public int BinarySearch(int index, int count, T item, IComparer<T> comparer)
+        {
+            if (count == 1)
+            {
+                if (comparer.Compare(array[index], item) == 0)
+                {
+                    return index;
+                }
+                else
+                {
+                    return -1;
+                }
+            }
+            int left = index;
+            int right = index + count - 1;
+            int middle = (left + right) / 2;
+            if (comparer.Compare(item, array[middle]) <= 0)
+            {
+                return BinarySearch(left, middle - left + 1, item, comparer);
+            }
+            else
+            {
+                return BinarySearch(middle + 1, right - middle, item, comparer);
+            }
+        }
+
+        public int BinarySearch(T item, IComparer<T> comparer)
+        {
+            return BinarySearch(0, Count, item, comparer);
+        }
+
+        public int BinarySearch(T item)
+        {
+            return BinarySearch(item, Comparer<T>.Default);
         }
 
         public void Clear()
         {
             array = new T[0];
             Count = 0;
-        }
-
-        public ReadOnlyCollection<T> AsReadOnly()
-        {
-            return new ReadOnlyCollection<T>(array);
         }
 
         public bool Contains(T item)
@@ -84,6 +112,239 @@ namespace MyList
                     return true;
             }
             return false;
+        }
+
+        public MyList<U> ConvertAll<U>(Converter<T, U> converter)
+        {
+            MyList<U> result = new MyList<U>(Count);
+            for(int i = 0; i < Count; i++)
+            {
+                result[i] = converter(array[i]);
+            }
+            return result;
+        }
+
+        public void CopyTo(T[] array)
+        {
+            CopyTo(array, 0);
+        }
+
+        public void CopyTo(T[] array, int arrayIndex)
+        {
+            CopyTo(0, array, arrayIndex, Count);
+        }
+
+        public void CopyTo(int index, T[] array, int arrayindex, int count)
+        {
+            Array.Copy(this.array, index, array, arrayindex, count);
+        }
+
+        public bool Exists(Predicate<T> predicate)
+        {
+            return !Find(predicate).Equals(default(T));
+        }
+
+        public T Find(Predicate<T> predicate)
+        {
+            for(int i = 0; i < Count; i++)
+            {
+                if (predicate(array[i]))
+                {
+                    return array[i];
+                }
+            }
+            return default(T);
+        }
+
+        public List<T> FindAll(Predicate<T> predicate)
+        {
+            var result = new List<T>();
+            for (int i = 0; i < Count; i++)
+            {
+                if (predicate(array[i]))
+                {
+                    result.Add(array[i]);
+                }
+            }
+            return result.Count == 0 ? null : result;
+        }
+
+        public int FindIndex(int startIndex, int count, Predicate<T> predicate)
+        {
+            for (int i = startIndex; i < startIndex + count; i++)
+            {
+                if (predicate(array[i]))
+                {
+                    return i;
+                }
+            }
+            return -1;
+        }
+
+        public int FindIndex(int startIndex, Predicate<T> predicate)
+        {
+            return FindIndex(startIndex, Count, predicate);
+        }
+
+        public int FindIndex(Predicate<T> predicate)
+        {
+            return FindIndex(0, Count, predicate);
+        }
+
+        public T FindLast(Predicate<T> predicate)
+        {
+            for(int i = Count - 1; i > 0; i--)
+            {
+                if (predicate(array[i]))
+                {
+                    return array[i];
+                }
+            }
+            return default(T);
+        }
+
+        public int FindLastIndex(int startIndex, int count, Predicate<T> predicate)
+        {
+            for (int i = startIndex + count - 1; i >= startIndex; i--)
+            {
+                if (predicate(array[i]))
+                {
+                    return i;
+                }
+            }
+            return -1;
+        }
+
+        public int FindLastIndex(int startIndex, Predicate<T> predicate)
+        {
+            return FindLastIndex(startIndex, Count, predicate);
+        }
+
+        public int FindLastIndex(Predicate<T> predicate)
+        {
+            return FindLastIndex(0, Count, predicate);
+        }
+
+        public void ForEach(Action<T> action)
+        {
+            for (int i = 0; i < Count; i++)
+            {
+                action(array[i]);
+            }
+        }
+
+        public MyList<T> GetRange(int index, int count)
+        {
+            MyList<T> result = new MyList<T>();
+            var resultArray = new T[count];
+            Array.Copy(array, index, resultArray, 0, count);
+            result.AddRange(resultArray);
+            return result;
+        }
+
+        public int IndexOf(T item)
+        {
+            return FindIndex(x => x.Equals(item));
+        }
+
+        public int IndexOf(T item, int index)
+        {
+            return FindIndex(index, x => x.Equals(item));
+        }
+
+        public int IndexOf(T item, int index, int count)
+        {
+            return FindIndex(index, count, x => x.Equals(item));
+        }
+
+        public void Insert(int index, T item)
+        {
+            //Add(default(T));
+            //Array.Copy(array, index, array, index + 1, Count - index);
+            //array[index] = item;
+            InsertRange(index, new T[] { item });
+        }
+
+        public void InsertRange(int index, IEnumerable<T> items)
+        {
+            AddRange(new List<T>(items.Count()));
+            Array.Copy(array, index, array, index + items.Count(), Count - index);
+            Array.Copy(items.ToArray(), 0, array, index, items.Count());
+        }
+
+        public int LastIndexOf(T item)
+        {
+            return FindLastIndex(x => x.Equals(item));
+        }
+
+        public int LastIndexOf(T item, int index)
+        {
+            return FindLastIndex(index, x => x.Equals(item));
+        }
+
+        public int LastIndexOf(T item, int index, int count)
+        {
+            return FindLastIndex(index, count, x => x.Equals(item));
+        }
+
+        public bool Remove(T item)
+        {
+            int index = -1;
+            for (int i = 0; i < Count; i++)
+            {
+                if (array[i].Equals(item))
+                {
+                    index = i;
+                    break;
+                }
+            }
+
+            if (index != -1)
+            {
+                RemoveAt(index);
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        public int RemoveAll(Predicate<T> predicate)
+        {
+            int removed = 0;
+            for (int i = 0; i < Count; i++)
+            {
+                if (predicate(array[i]))
+                {
+                    removed++;
+                }
+                else
+                {
+                    array[i - removed] = array[i];
+                }
+            }
+            Count -= removed;
+            return removed;
+        }
+
+        public void RemoveAt(int index)
+        {
+            RemoveRange(index, 1);
+        }
+
+        public void RemoveRange(int index, int count)
+        {
+            Array.Copy(array, index + count, array, index, Count - index - count);
+            Count -= count;
+        }
+
+        public int Reverse(int index, int count)
+
+        private void Test()
+        {
+            List<char> list = new List<char>();
+            list.;
         }
     }
 }
--- TrainingConsole/MyList/Program.cs
+++ TrainingConsole/MyList/Program.cs
@@ -1,21 +1,23 @@
-﻿using System;
+﻿using Sorts;
+using System;
+using System.ComponentModel;
 
 namespace MyList
 {
     class Program
     {
-        public static void WriteElement(int value)
+        public static void WriteElement<T>(T value)
         {
             Console.Write($"{value} ");
         }
-        public static void OutputMyList(MyList<int> myList)
+        public static void OutputMyList<T>(MyList<T> myList)
         {
             Console.WriteLine($"myList size: {myList.Count}");
-            Action<int> myDelegate = WriteElement;
+            Action<T> myDelegate = WriteElement;
             myList.ForEach(myDelegate);
             Console.WriteLine();
         }
-        
+
         static void Main(string[] args)
         {
             var myList1 = new MyList<int>();
@@ -38,6 +40,40 @@ namespace MyList
 
             Console.WriteLine(myList3.Contains(9));
             OutputMyList(myList3);
+
+            var array = Tools.CreateUnsortedArray(20);
+            Sorts.Sorts.MergeSort(array);
+            myList1.AddRange(array);
+            var aim = (new Random()).Next(20);
+            Console.WriteLine($"Aim: {aim}");
+            OutputMyList(myList1);
+            Console.WriteLine(myList1.BinarySearch(aim));
+
+            OutputMyList(myList1.ConvertAll(value => (char)value));
+
+            array = new int[20];
+            myList1.CopyTo(array);
+            myList1.AddRange(array);
+            OutputMyList(myList1);
+
+            Console.WriteLine("Exists: " + myList1.Exists(x => x == 10));
+            Console.WriteLine("Find: " + myList1.Find(x => x == 10));
+            Console.WriteLine("FindLast: " + myList1.FindLast(x => x == 10));
+            Console.WriteLine("FindAll.Count: " + myList1.FindAll(x => x == 10)?.Count);
+            Console.WriteLine("FindIndex: " + myList1.FindIndex(x => x == 10));
+            Console.WriteLine("FindLastIndex: " + myList1.FindLastIndex(x => x == 10));
+            Console.WriteLine("IndexOf: " + myList1.IndexOf(10));
+            Console.WriteLine("LastIndexOf: " + myList1.LastIndexOf(10));
+
+            OutputMyList(myList1.GetRange(3, 4));
+            myList1.Insert(2, -1);
+            OutputMyList(myList1);
+
+            myList1.Remove(-1);
+            myList1.RemoveRange(37, 2);
+            myList1.RemoveAt(0);
+            myList1.RemoveAll(x => x == 8);
+            OutputMyList(myList1);
         }
     }
 }
